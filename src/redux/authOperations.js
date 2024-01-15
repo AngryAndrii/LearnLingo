456d199b784e9980a180defe648dd56df8d094c3
Andrii Chychkan <mywork1728@gmail.com>
2024-01-15 17:19:27 +0200

    added redux store

---
+++ src/redux/authOperations.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
+import { auth } from '../config/firebase';
+
+export const registerUser = createAsyncThunk(
+  'userAuth/register',
+  async (body, thunkAPI) => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        body.email,
+        body.password,
+        body.name
+      );
+      await updateProfile(auth.currentUser, { displayName: body.name });
+
+      const { uid, displayName, email } = auth.currentUser;
+
+      return { uid, displayName, email };
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+);
+
+// const loginuser = createAsyncThunk(
+//   'userAuth/login',
+//   async (userId, thunkAPI) => {
+//     const response = await userAPI.fetchById(userId);
+//     return response.data;
+//   }
+// );
+
+// const logOutUser = createAsyncThunk(
+//   'userAuth/logout',
+//   async (userId, thunkAPI) => {
+//     const response = await userAPI.fetchById(userId);
+//     return response.data;
+//   }
+// );
+
+// dispatch(fetchUserById(123));
