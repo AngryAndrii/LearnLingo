6df02a70c6e3010f23d7b53c10eeab10a998d187
Andrii Chychkan <mywork1728@gmail.com>
2024-01-17 13:01:50 +0200

    added main page

---
+++ src/pages/home/Home.styled.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const HomePage = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  img {
+    max-width: 100%;
+  }
+  .advantages {
+    border: 1.5px dashed var(--secondary-color);
+    border-radius: 30px;
+    padding: 20px 20px;
+  }
+  .adv-content {
+    display: flex;
+    flex-direction: column;
+    row-gap: 10px;
+    p {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      font-size: 14px;
+      font-weight: 400;
+    }
+    span {
+      font-size: 28px;
+      font-weight: 500;
+      white-space: nowrap;
+    }
+  }
+`;
+
+export default HomePage;
