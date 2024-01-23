b16d30255b80cf07a16a79fbf05fcebcb3501f27
Andrii Chychkan <mywork1728@gmail.com>
2024-01-23 16:41:21 +0200

    added redus persistsotore

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './globalStyled.js';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
       <GlobalStyle />
     </BrowserRouter>
