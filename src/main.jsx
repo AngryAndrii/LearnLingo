15501183884cf64665c91f220bda637769a7cb57
Andrii Chychkan <mywork1728@gmail>
2024-01-23 22:15:46 +0200

    fixed redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import { GlobalStyle } from './globalStyled.js';
-import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./globalStyled.js";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          {/* <BrowserRouter basename="/learnlingo"> */}
           <App />
-        </PersistGate>
-      </Provider>
-      <GlobalStyle />
-    </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+    <GlobalStyle />
   </React.StrictMode>
 );
