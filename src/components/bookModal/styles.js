dc1ef77152fe52aeb9c37acabd3fdbe15f6d91e4
Andrii Chychkan <mywork1728@gmail.com>
2024-04-10 16:33:11 +0300

    added book validation

---
+++ src/components/bookModal/styles.js
@@ -0,0 +1,18 @@
+export const style = {
+  content: {
+    position: 'relative',
+    top: '55%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80vw',
+    height: '100vh',
+    paddingTop: 44,
+    paddingBottom: 44,
+    borderRadius: 30,
+    maxWidth: 570,
+    overflow: 'scroll',
+  },
+};
